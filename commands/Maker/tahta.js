cf5d6038006bf554939c9e8892fe542f7f621781
Farhannnnn <74123134+Fxc7@users.noreply.github.com>
2023-06-13 13:34:12 +0700

    update tahta maker
    Signed-off-by: Farhannnnn <74123134+Fxc7@users.noreply.github.com>

---
+++ commands/Maker/tahta.js
@@ -0,0 +1,20 @@
+'use strict';
+
+export default {
+    views: ['tahta'], // view for message in  menu
+    command: /^tahta$/i, //another command.
+    description: 'Create Tahta quotes images',
+    usage: '%cmd% xcoders',
+    query: true,
+    execute: async ({ xcoders, m, x, apikeys, query, waitingMessage, errorMessage, host }, { getJson }, { addHitCommand }) => {
+      try {
+        const data = await getJson(`${host}/api/maker/tahta?text=${query}&result_type=json&apikey=${apikeys}`);
+        const result = Buffer.from(data, 'buffer');
+        await waitingMessage(m.chat);
+        addHitCommand('Tahta Maker', true);
+        return xcoders.sendMessage(m.chat, { image: result, caption: response.success }, { quoted: x });
+      } catch (error) {
+        return errorMessage(m.chat, error, 'Tahta Maker');
+      }
+    }
+  };
